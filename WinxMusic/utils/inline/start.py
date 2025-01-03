0c8a1721466c5f937d4007d456b256fe7abc771c
Gabriel Maia <gabrielmaialva33@gmail.com>
2025-01-03 15:07:41 -0300

    chore: disable GitHub repository button in the start menu.
    Commented out the inline button linking to the GitHub repository. This prevents the button from displaying while retaining the code for future use if needed.

--- WinxMusic/utils/inline/start.py
+++ WinxMusic/utils/inline/start.py
@@ -67,7 +67,7 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
         buttons.append(
             [
                 InlineKeyboardButton(text=_["S_B_7"], user_id=OWNER),
-                InlineKeyboardButton(text=_["S_B_6"], url=f"{GITHUB_REPO}"),
+                # InlineKeyboardButton(text=_["S_B_6"], url=f"{GITHUB_REPO}"),
             ]
         )
     else:
