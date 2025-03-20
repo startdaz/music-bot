3e8aa4be3b7bc1d223c9e488f9e1b9fc8306d646
Gabriel Maia <gabrielmaialva33@gmail.com>
2025-03-19 22:41:10 -0300

    chore: simplify command handling in block.py by using direct command imports

--- WinxMusic/plugins/sudo/block.py
+++ WinxMusic/plugins/sudo/block.py
@@ -1,4 +1,3 @@
-from pyrogram import filters, Client
 from pyrogram.types import Message
 
 from WinxMusic import app
@@ -6,16 +5,12 @@ from WinxMusic.misc import SUDOERS
 from WinxMusic.utils.database import add_gban_user, remove_gban_user
 from WinxMusic.utils.decorators.language import language
 from config import BANNED_USERS
-from strings import command, get_command
-
-BLOCK_COMMAND = get_command("BLOCK_COMMAND")
-UNBLOCK_COMMAND = get_command("UNBLOCK_COMMAND")
-BLOCKED_COMMAND = get_command("BLOCKED_COMMAND")
+from strings import command
 
 
-@app.on_message(filters.command(BLOCK_COMMAND) & SUDOERS)
+@app.on_message(command("BLOCK_COMMAND") & SUDOERS)
 @language
-async def useradd(_client: Client, message: Message, _):
+async def useradd(client, message: Message, _):
     if not message.reply_to_message:
         if len(message.command) != 2:
             return await message.reply_text(_["general_1"])
@@ -40,7 +35,7 @@ async def useradd(_client: Client, message: Message, _):
     )
 
 
-@app.on_message(filters.command(UNBLOCK_COMMAND) & SUDOERS)
+@app.on_message(command("UNBLOCK_COMMAND") & SUDOERS)
 @language
 async def userdel(client, message: Message, _):
     if not message.reply_to_message:
@@ -64,7 +59,7 @@ async def userdel(client, message: Message, _):
     await message.reply_text(_["block_4"])
 
 
-@app.on_message(filters.command(BLOCKED_COMMAND) & SUDOERS)
+@app.on_message(command("BLOCKED_COMMAND") & SUDOERS)
 @language
 async def sudoers_list(client, message: Message, _):
     if not BANNED_USERS:
@@ -84,19 +79,3 @@ async def sudoers_list(client, message: Message, _):
         return await mystic.edit_text(_["block_5"])
     else:
         return await mystic.edit_text(msg)
-
-
-__MODULE__ = "B-list"
-__HELP__ = f"""
-<b>✧ {command("BLACKLISTCHAT_COMMAND")}</b> [ID do chat] - Bloquear qualquer chat de usar o Bot de Música.
-<b>✧ {command("WHITELISTCHAT_COMMAND")}</b> [ID do chat] - Desbloquear qualquer chat da lista de bloqueio para usar o Bot de Música.
-<b>✧ {command("BLACKLISTEDCHAT_COMMAND")}</b> - Verificar todos os chats bloqueados.
-
-<b>✧ {command("BLOCK_COMMAND")}</b> [Nome de usuário ou responder a um usuário] - Impede um usuário de usar comandos do bot.
-<b>✧ {command("UNBLOCK_COMMAND")}</b> [Nome de usuário ou responder a um usuário] - Remove um usuário da lista de bloqueio do bot.
-<b>✧ {command("BLOCKED_COMMAND")}</b> - Verificar a lista de usuários bloqueados.
-
-<b>✧ {command("GBAN_COMMAND")}</b> [Nome de usuário ou responder a um usuário] - Banir um usuário de todos os chats atendidos e impedir que ele use seu bot.
-<b>✧ {command("UNGBAN_COMMAND")}</b> [Nome de usuário ou responder a um usuário] - Remove um usuário da lista de banimento global e permite que ele use seu bot.
-<b>✧ {command("GBANNED_COMMAND")}</b> - Verificar a lista de usuários banidos globalmente.
-"""
