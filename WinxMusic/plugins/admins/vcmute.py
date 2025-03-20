3b7fe9e5bc1820873f18cb6cf09ae37e0d1b7d46
Gabriel Maia <gabrielmaialva33@gmail.com>
2025-03-19 23:05:26 -0300

    chore: streamline command handling for mute and unmute functionality in vcmute.py

--- WinxMusic/plugins/admins/vcmute.py
+++ WinxMusic/plugins/admins/vcmute.py
@@ -1,22 +1,19 @@
-from pyrogram import filters, Client
+from pyrogram import filters
 from pyrogram.types import Message
 
 from WinxMusic import app
 from WinxMusic.core.call import Winx
 from WinxMusic.utils.database import is_muted, mute_off, mute_on
 from WinxMusic.utils.decorators import admin_rights_check
-from config import BANNED_USERS, PREFIXES
-from strings import get_command
-
-MUTE_COMMAND = get_command("MUTE_COMMAND")
-UNMUTE_COMMAND = get_command("UNMUTE_COMMAND")
+from config import BANNED_USERS
+from strings import command
 
 
-@app.on_message(filters.command(MUTE_COMMAND, PREFIXES) & filters.group & ~BANNED_USERS)
+@app.on_message(command("MUTE_COMMAND") & filters.group & ~BANNED_USERS)
 @admin_rights_check
-async def mute_admin(_client: Client, message: Message, _, chat_id: int):
+async def mute_admin(cli, message: Message, _, chat_id):
     if not len(message.command) == 1 or message.reply_to_message:
-        return await message.reply_text(_["general_2"])
+        return
     if await is_muted(chat_id):
         return await message.reply_text(_["admin_5"], disable_web_page_preview=True)
     await mute_on(chat_id)
@@ -26,11 +23,11 @@ async def mute_admin(_client: Client, message: Message, _, chat_id: int):
     )
 
 
-@app.on_message(filters.command(UNMUTE_COMMAND, PREFIXES) & filters.group & ~BANNED_USERS)
+@app.on_message(command("UNMUTE_COMMAND") & filters.group & ~BANNED_USERS)
 @admin_rights_check
-async def unmute_admin(_client: Client, message: Message, _, chat_id: int):
+async def unmute_admin(Client, message: Message, _, chat_id):
     if not len(message.command) == 1 or message.reply_to_message:
-        return await message.reply_text(_["general_2"])
+        return
     if not await is_muted(chat_id):
         return await message.reply_text(_["admin_7"], disable_web_page_preview=True)
     await mute_off(chat_id)
