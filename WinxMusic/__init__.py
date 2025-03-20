174a67ad7433a71eea893790b500568bf4913ed9
Gabriel Maia <gabrielmaialva33@gmail.com>
2025-03-19 22:16:32 -0300

    chore: update bot name to WinxMusic and adjust max concurrent transmissions

--- WinxMusic/__init__.py
+++ WinxMusic/__init__.py
@@ -1,39 +1,30 @@
+import config
 from WinxMusic.core.bot import WinxBot
 from WinxMusic.core.dir import dirr
 from WinxMusic.core.git import git
 from WinxMusic.core.userbot import Userbot
 from WinxMusic.misc import dbb, heroku, sudo
-from .core.cookies import save_cookies
 
 from .logging import LOGGER
-import config
 
-# Bot Client
+# Pyrogram Client
+
 app = WinxBot(
-    "FloraMusic",
+    "WinxMusic",
     api_id=config.API_ID,
     api_hash=config.API_HASH,
     bot_token=config.BOT_TOKEN,
     sleep_threshold=240,
-    max_concurrent_transmissions=6,
+    max_concurrent_transmissions=5,
     workers=50,
 )
 
-# Assistant Client
 userbot = Userbot()
-
-for i, session in enumerate(config.STRING_SESSIONS, start=1):
-    userbot.add(
-        f"FloraString{i}",
-        api_id=config.API_ID,
-        api_hash=config.API_HASH,
-        session_string=session.strip(),
-    )
 
 # Directories
 dirr()
 
-# # Check Git Updates
+# Check Git Updates
 git()
 
 # Initialize Memory DB
@@ -48,5 +39,4 @@ sudo()
 from .platforms import PlaTForms
 
 Platform = PlaTForms()
-
 HELPABLE = {}
