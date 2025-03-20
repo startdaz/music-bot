fd2c0811275827b5e9927bf73a464d6a20932751
Gabriel Maia <gabrielmaialva33@gmail.com>
2025-03-19 23:00:53 -0300

    chore: refactor play.py for improved readability and consistency in error handling

--- WinxMusic/utils/decorators/play.py
+++ WinxMusic/utils/decorators/play.py
@@ -1,18 +1,8 @@
-import asyncio
+from pyrogram.errors import ChannelPrivate
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-from pyrogram import Client
-from pyrogram.errors import (
-    ChannelsTooMuch,
-    ChatAdminRequired,
-    FloodWait,
-    InviteRequestSent,
-    UserAlreadyParticipant, ChannelPrivate,
-)
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
-
-from WinxMusic import app, Platform
+from WinxMusic import Platform, app
 from WinxMusic.core.call import Winx
-from WinxMusic.core.userbot import assistants
 from WinxMusic.misc import SUDOERS
 from WinxMusic.utils.database import (
     get_assistant,
@@ -24,84 +14,16 @@ from WinxMusic.utils.database import (
     is_commanddelete_on,
     is_maintenance,
     is_served_private_chat,
-    set_assistant,
 )
 from WinxMusic.utils.inline import botplaylist_markup
-from config import PLAYLIST_IMG_URL, PRIVATE_BOT_MODE
-from config import SUPPORT_GROUP as SUPPORT_CHAT
-from config import adminlist
+from config import PLAYLIST_IMG_URL, PRIVATE_BOT_MODE, adminlist
 from strings import get_string
 
 links = {}
 
 
-async def join_chat(message: Message, chat_id: int, _, myu: Message = None, attempts=1):
-    max_attempts = len(assistants) - 1  # Set the maximum number of attempts
-    userbot = await get_assistant(chat_id)
-
-    if chat_id in links:
-        invite_link = links[chat_id]
-    else:
-        if message.chat.username:
-            invite_link = message.chat.username
-            try:
-                await userbot.resolve_peer(invite_link)
-            except Exception:
-                pass
-        else:
-            try:
-                invite_link = await app.export_chat_invite_link(message.chat.id)
-            except ChatAdminRequired:
-                return await myu.edit(_["call_1"])
-            except Exception as e:
-                return await myu.edit(_["call_3"].format(app.mention, type(e).__name__))
-
-        if invite_link.startswith("https://t.me/+"):
-            invite_link = invite_link.replace(
-                "https://t.me/+", "https://t.me/joinchat/"
-            )
-        links[chat_id] = invite_link
-
-    try:
-        await asyncio.sleep(1)
-        await userbot.join_chat(invite_link)
-    except InviteRequestSent:
-        try:
-            await app.approve_chat_join_request(chat_id, userbot.id)
-        except Exception as e:
-            return await myu.edit(_["call_3"].format(type(e).__name__))
-        await asyncio.sleep(1)
-        await myu.edit(_["call_6"].format(app.mention))
-    except UserAlreadyParticipant:
-        pass
-    except ChannelsTooMuch:
-        if attempts <= max_attempts:
-            userbot = await set_assistant(chat_id)
-            return await join_chat(message, chat_id, _, myu, attempts + 1)
-        else:
-            return await myu.edit(_["call_9"].format(SUPPORT_CHAT))
-    except FloodWait as e:
-        time = e.value
-        if time < 20:
-            await asyncio.sleep(time)
-            return await join_chat(message, chat_id, _, myu, attempts + 1)
-        else:
-            if attempts <= max_attempts:
-                userbot = await set_assistant(chat_id)
-                return await join_chat(message, chat_id, _, myu, attempts + 1)
-
-            return await myu.edit(_["call_10"].format(time))
-    except Exception as e:
-        return await myu.edit(_["call_3"].format(type(e).__name__))
-
-    try:
-        await myu.delete()
-    except Exception:
-        pass
-
-
-def play_wrapper(command: callable):
-    async def wrapper(client: Client, message: Message):
+def play_wrapper(command):
+    async def wrapper(client, message):
         language = await get_lang(message.chat.id)
         _ = get_string(language)
         if message.sender_chat:
@@ -109,7 +31,7 @@ def play_wrapper(command: callable):
                 [
                     [
                         InlineKeyboardButton(
-                            text="Como resolver isso?",
+                            text="How to Fix ?",
                             callback_data="AnonymousAdmin",
                         ),
                     ]
@@ -124,7 +46,7 @@ def play_wrapper(command: callable):
         if PRIVATE_BOT_MODE == str(True):
             if not await is_served_private_chat(message.chat.id):
                 await message.reply_text(
-                    "**BOT DE MÚSICA PRIVADO**\n\nSomente para chats autorizados pelo dono. Peça ao meu dono para permitir o seu chat primeiro."
+                    "**PRIVATE MUSIC BOT**\n\nOnly For Authorized chats from the owner ask my owner to allow your chat first."
                 )
                 return await app.leave_chat(message.chat.id)
         if await is_commanddelete_on(message.chat.id):
@@ -170,7 +92,7 @@ def play_wrapper(command: callable):
             is_call_active = (await app.get_chat(chat_id)).is_call_active
             if not is_call_active:
                 return await message.reply_text(
-                    "**Nenhum chat de vídeo ativo encontrado**\n\nPor favor, certifique-se de que você iniciou o chat de voz."
+                    "**No active video chat found **\n\nPlease make sure you started the voicechat."
                 )
         except Exception:
             pass
@@ -198,21 +120,21 @@ def play_wrapper(command: callable):
             fplay = True
         else:
             fplay = None
+        if await is_active_chat(chat_id):
+            userbot = await get_assistant(message.chat.id)
+            # Getting all members id that in voicechat
+            try:
+                call_participants_id = [
+                    member.chat.id
+                    async for member in userbot.get_call_members(chat_id)
+                    if member.chat
+                ]
+                # Checking if assistant id not in list so clear queues and remove active voice chat and process
 
-            if await is_active_chat(chat_id):
-                userbot = await get_assistant(message.chat.id)
-                # Getting all members id that in voicechat
-                try:
-                    call_participants_id = [
-                        member.chat.id
-                        async for member in userbot.get_call_members(chat_id)
-                        if member.chat
-                    ]
-                    # Checking if assistant id not in list so clear queues and remove active voice chat and process
-                    if not call_participants_id or userbot.id not in call_participants_id:
-                        await Winx.stop_stream(chat_id)
-                except ChannelPrivate:
-                    pass
+                if not call_participants_id or userbot.id not in call_participants_id:
+                    await Winx.stop_stream(chat_id)
+            except ChannelPrivate:
+                pass
 
         return await command(
             client,
