020da956229932e098775a0149a46bf40d25be89
Gabriel Maia <gabrielmaialva33@gmail.com>
2025-03-19 22:37:19 -0300

    chore: simplify command handling and update response messages in active.py

--- WinxMusic/utils/decorators/admins.py
+++ WinxMusic/utils/decorators/admins.py
@@ -1,11 +1,6 @@
 from pyrogram import Client
 from pyrogram.enums import ChatMemberStatus, ChatType
-from pyrogram.types import (
-    InlineKeyboardButton,
-    InlineKeyboardMarkup,
-    Message,
-    CallbackQuery,
-)
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message, CallbackQuery
 
 from WinxMusic import app
 from WinxMusic.misc import SUDOERS
@@ -113,7 +108,10 @@ def admin_actual(mystic: callable):
                     message.chat.id, message.from_user.id
                 )
 
-                if member.status not in [ChatMemberStatus.ADMINISTRATOR, ChatMemberStatus.OWNER] or (
+                if member.status not in [
+                    ChatMemberStatus.ADMINISTRATOR,
+                    ChatMemberStatus.OWNER,
+                ] or (
                         member.privileges is None
                         or not member.privileges.can_manage_video_chats
                 ):
